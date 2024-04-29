74ecea60e9dd54bb6ae1b17ac02f6fe13d65225c
flpalacios93@gmail.comgit config --global user.name flap93git config --global user.name flpalacios93@gmail.com <flpalacios93@gmail.com>
2024-04-29 22:06:27 +0200

    Initial commit to test eclipse with git hub

---
+++ BancoSerializable/src/constantesBD/ConstantesBD.java
@@ -0,0 +1,7 @@
+package constantesBD;
+
+public class ConstantesBD {
+	public static final String URL="jdbc:mysql://localhost/banco";
+	public static final String USUARIO="root";
+	public static final String PWD="";
+}
